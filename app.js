ffe352a600a094f2368d6d13d69b75fc2ab7a269
Noah Heague <noahheague@gmail.com>
2018-11-12 14:00:24 +1100

    Public interface

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const server = require('http').Server(app);
 
-server.listen(3000);
+server.listen(3000, '0.0.0.0');
 
 app.use(express.static('public'))
 
